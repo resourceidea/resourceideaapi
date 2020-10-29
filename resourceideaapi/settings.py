8dfc484ff4fbcef11da1c96095cd559ad487edac
Joseph Serunjogi <jserunjogi@touchpointmed.com>
2020-10-30 00:46:32 +0300

    Allow development CORS host

--- resourceideaapi/settings.py
+++ resourceideaapi/settings.py
@@ -165,7 +165,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5000",
     "https://127.0.0.1:5001",
     "http://localhost:5000",
-    "https://localhost:5001"
+    "https://localhost:5001",
+    "https://localhost:44357"
 ]
 
 # Sentry monitoring setup
